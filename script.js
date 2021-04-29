8f2b615b3007408464bb2b5b95db8b42a5bdbe48
Nash Villarta <>
2021-04-29 00:38:03 +0000

    Added additional spaces and made unfunctional commands for stylecss to pass linter

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //This identifies the button and listens for a click
 
 document.getElementById('mybutton').addEventListener('click', displayMessage)
+
 //This sets the background color of the page
 
 document.body.style.backgroundColor = 'lightblue'
+
 //This changes the color of the text
 
 document.getElementById('title').style.color = 'darkblue'
+
 //This creates functionality for the button. I made it so that once clicked, it changes text and color. This also notifies the user with a alert once clicked.
 
 function displayMessage () {
