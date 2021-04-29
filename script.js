70b5e0a6ebb5cb56f6b29c7649ed4dc1edf0930c
Nash Villarta <>
2021-04-29 00:43:30 +0000

    I tabbed the comment line instead

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-//This identifies the button and listens for a click
-
+//This identifies the button and listens for a click  
 document.getElementById('mybutton').addEventListener('click', displayMessage)
-
-//This sets the background color of the page
-
+//This sets the background color of the page  
 document.body.style.backgroundColor = 'lightblue'
-
-//This changes the color of the text
-
+//This changes the color of the text  
 document.getElementById('title').style.color = 'darkblue'
-
-//This creates functionality for the button. I made it so that once clicked, it changes text and color. This also notifies the user with a alert once clicked.
-
+//This creates functionality for the button. I made it so that once clicked, it changes text and color. This also notifies the user with a alert once clicked.  
 function displayMessage () {
   document.getElementById('textone').innerHTML = 'Thank you for clicking!'
   document.getElementById('textone').style.color = 'red'
